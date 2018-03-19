e148f53d30b12928dce648fc20397202722deff7
wanyibo <1078437881@qq.com>
2018-03-19 17:46:12 +0800

    BaseClass

---
+++ app/src/main/java/com/yshare/baselib/retroft/ApiException.java
@@ -0,0 +1,21 @@
+package com.yshare.baselib.retroft;
+
+/**
+ * Created by Administrator on 2018/3/19.
+ *
+ *
+ */
+
+public class ApiException extends RuntimeException{
+
+    private int code;
+
+    public ApiException(Throwable throwable,int code){
+        super(throwable);
+        this.code = code;
+    }
+
+    public ApiException(String message){
+        super(new Throwable(message));
+    }
+}
